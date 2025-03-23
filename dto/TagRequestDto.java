6bfd3ca39b3001fad654c1f4987d1d426a2ea289
BhushanMahadik <149048349+BhushanMahadik@users.noreply.github.com>
2025-03-23 19:58:25 +0530

    Tag Dto added succesfully

---
+++ dto/TagRequestDto.java
@@ -0,0 +1,12 @@
+package com.postify.main.dto.TagDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagRequestDto {
+    private String name;
+}
---
+++ dto/TagResponseDto.java
@@ -0,0 +1,13 @@
+package com.postify.main.dto.TagDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagResponseDto {
+    private int id;
+    private String name;
+}
