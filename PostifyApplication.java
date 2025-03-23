72df1e01f07321e4ee612cd25663a376613ed543
BhushanMahadik <149048349+BhushanMahadik@users.noreply.github.com>
2025-03-23 20:42:41 +0530

    Resource file uploadded

---
+++ PostifyApplication.java
@@ -0,0 +1,33 @@
+package com.postify.main;
+
+import com.postify.main.services.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+	@Autowired
+	UserService userService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostifyApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		try{
+			System.out.println("Creating super user....");
+			userService.createSuperUser("Bhushan","Bhushan@1234","mahadikbhushan768@gmail.com");
+			System.out.println("Super user created.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error creating super user: "+e.getMessage());
+		}
+	}
+
+
+
+}
